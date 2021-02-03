5076e8afd0069707412240b20c7fe7885fe097ef
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-02-03 16:08:55 +0800

    Update compact_history.py

--- compact_history.py
+++ compact_history.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 #
-#
 # Copyright (c) 2017, Neil Booth
 #
 # All rights reserved.
 #
 # See the file "LICENCE" for information about the copyright
+
 # and warranty status of this software.
+
 
 
 '''Script to compact the history database.  This should save space and
 will reset the flush counter to a low number, avoiding overflow when
 the flush count reaches 65,536.
 
+
 This needs to lock the database so ElectrumX must not be running -
 shut it down cleanly first.
+
 
 It is recommended you run this script with the same environment as
 ElectrumX.  However it is intended to be runnable with just
 DB_DIRECTORY and COIN set (COIN defaults as for ElectrumX).
 
+
 If you use daemon tools, you might run this script like so:
 
+
    envdir /path/to/the/environment/directory ./compact_history.py
+
 
 Depending on your hardware this script may take up to 6 hours to
 complete; it logs progress regularly.
@@ -33,6 +39,7 @@ up where it left off.  However, if you restart ElectrumX without
 running the compaction to completion, it will not benefit and
 subsequent compactions will restart from the beginning.
 '''
+
 
 
 import logging
@@ -46,27 +53,34 @@ from server.db import DB
 
 
 
+
 def compact_history():
     if sys.version_info < (3, 6):
         raise RuntimeError('Python >= 3.6 is required to run ElectrumX')
 
+      
     environ['DAEMON_URL'] = ''   # Avoid Env erroring out
     env = Env()
     db = DB(env)
 
+   
     assert not db.first_sync
     # Continue where we left off, if interrupted
     if db.comp_cursor == -1:
         db.comp_cursor = 0
 
+      
     db.comp_flush_count = max(db.comp_flush_count, 1)
     limit = 8 * 1000 * 1000
 
+      
     while db.comp_cursor != -1:
         db._compact_history(limit)
 
+         
 
 def main():
+   
     logging.basicConfig(level=logging.INFO)
     logging.info('Starting history compaction...')
     try:
@@ -81,3 +95,5 @@ def main():
          
 if __name__ == '__main__':
     main()
+
+      
