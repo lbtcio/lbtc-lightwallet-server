4799f2b22f7c6f2e15ebb06c9d22b1d4f99d2419
WuDing <44403890+KingWuDing@users.noreply.github.com>
2021-04-01 10:34:50 +0800

    Update compact_history.py

--- compact_history.py
+++ compact_history.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 #
-
 # Copyright (c) 2017, Neil Booth
 #
-
 # All rights reserved.
 #
-
 # See the file "LICENCE" for information about the copyright
 
 # and warranty status of this software.
-
 
 '''Script to compact the history database.  This should save space and
 will reset the flush counter to a low number, avoiding overflow when
